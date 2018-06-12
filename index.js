8ba8ade86d3b17a7c59328b8e7da484a65fe9ac1
Pavel Drankov <titantins@gmail.com>
2018-06-12 23:33:36 +0300

    proxy server is done

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+var net = require('net');
 
-
-// create a webscoket server on port 8080
+// create a webscoket server on port 10023
 var WebSocketServer = require('ws').Server,
-wss = new WebSocketServer({port: 8080})
+wss = new WebSocketServer({port: 10023})
 wss.on('connection', function (ws) {
+
+	var client = new net.Socket();
+	client.connect(10001, 'ec2-35-180-30-241.eu-west-3.compute.amazonaws.com', function() {
+		console.log('Connected');
+		ws.send("connected")
+	});
+
+	client.on('data', function(data) {
+		console.log('Received: ' + data);
+		ws.send(data)
+	});
+
+	client.on('close', function() {
+		ws.send("closed")
+	})
+
   ws.on('message', function (message) {
-    console.log('received: %s', message)
+    client.write(message)
   })
-  setInterval(
-    () => ws.send(`${new Date()}`),
-    1000
-  )
 })
